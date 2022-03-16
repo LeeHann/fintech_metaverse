19b1a9225b8cf61ba82f1f59aa3b9a6c8e425114
LeeHann <0104ihn@naver.com>
2022-03-17 02:59:47 +0900

    맵 세팅

--- Assets/02_Scripts/Character/CharacterMovement.cs
+++ Assets/02_Scripts/Character/CharacterMovement.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Movement")]
+    public float speed = 3f;
+    private Vector3 vel;
+    private bool isRun;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private Rigidbody characterRigidbody;
+    private Animator animator;
+    
+    void Start(){
+        characterRigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+ 
+    void Update(){
+        vel.x = Input.GetAxis("Horizontal");                // 입력
+        vel.z = Input.GetAxis("Vertical");
+        isRun = Input.GetKey(KeyCode.LeftShift);
+        if (isRun)  speed = 4f;
+        else speed = 3f;
+
+        animator.SetBool("isWalk", vel != Vector3.zero);    // 걷기 애니메이션
+        animator.SetBool("isRun", isRun);                   // 뛰기 애니메이션
+    }
+    
+    private void FixedUpdate() {
+        // vel.y = 0;
+        // transform.LookAt(transform.position + vel);         // 방향
+
+        vel = vel * speed;
+        vel.y = characterRigidbody.velocity.y;
+        characterRigidbody.velocity = vel;                  // 위치 이동
     }
 }
